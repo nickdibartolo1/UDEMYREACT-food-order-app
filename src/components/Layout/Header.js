7a2f8fc7d8b4937715565fdbba873caac8256a30
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-06-12 11:51:17 -0400

    began component/css setup for project

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import HeaderCartButton from './HeaderCartButton';
+import meals from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>Meals</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={meals} alt="meals" />
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header
