b2a6f84f5dbb9badc757b40ef34a5c7d7f2e5b77
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-06-13 16:51:34 -0400

    added a card in the UI, created the mealItem subfolder, trimmed components in Meals folder

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './MealItemForm.module.css';
+
+
+const MealItemForm = () => {
+  return (
+    <div>MealItemForm</div>
+  )
+}
+
+export default MealItemForm
