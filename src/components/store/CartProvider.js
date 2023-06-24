f3d149cfd0508cfe7607dac0a537a5858ee9dd66
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-06-24 18:57:19 -0400

    added functionality and state to the Cart Button that updated correctly based upon user clicks

--- src/components/store/CartProvider.js
+++ src/components/store/CartProvider.js
@@ -1,18 +1,45 @@
-import React from 'react'
-import CartContext from './cart-context'
+import React, { useReducer } from 'react';
+import CartContext from './cart-context';
+
+//action is dispatched by you, state is last state snapshot managed by reducer.
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_CART_ITEM') {
+        //return a new array with concat()
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return{
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+    return
+}
+
+const defaultCart = {
+    items: [],
+    totalAmount: 0
+};
 
 const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCart);
 
     const addCartItemHandler = (item) => {
-
+        dispatchCartAction({
+            type: 'ADD_CART_ITEM',
+            item: item,
+        })
     }
     const removeCartItemHandler = (id) => {
+        dispatchCartAction({
+            item: 'REMOVE_CART_ITEM',
+            id: id
+        })
 
     }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addCartItemHandler,
         removeItem: removeCartItemHandler,
     }
